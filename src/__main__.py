390fb22135beb1a46509c16560af0ed2f9db046b
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-02-14 15:51:02 +0530

    Create __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,26 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from src import app, ub, LOGGER
+from src.modules import ALL_MODULES
+
+
+async def eval_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module(f"src.modules.{all_module}")
+    LOGGER.info(f"Successfully loaded {len(ALL_MODULES)}.")
+
+    LOGGER.info("Bot Started")
+    await idle()
+
+    try:
+        await app.stop()
+    except:
+        pass
+    LOGGER.info("» Stopping! GoodBye")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(eval_bot())
