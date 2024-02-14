cac493de1199d61b83b91938efad73e783c5328c
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-02-14 17:21:23 +0530

    Create start.py

---
+++ src/modules/start.py
@@ -0,0 +1,9 @@
+from pyrogram.types import Message
+from pyrogram import filters
+
+from src import app
+
+@app.on_message(filters.command("start"))
+async def strt(app, message: Message):
+  await message.reply_photo(photo="https://graph.org/file/6be5258519aab54f2c2e8.jpg", caption=f"{app.me.mention} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ.", 
+                            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{app.me.username}?startgroup=true")]]))
